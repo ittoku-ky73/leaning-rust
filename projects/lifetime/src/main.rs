ca42791cd8505fed66ccc8396f7af091fb1b2e45
ittokun <73953416+ittokun@users.noreply.github.com>
2023-02-25 21:20:17 +0900

    Finished Lifetime

--- projects/lifetime/src/main.rs
+++ projects/lifetime/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn main() {
     // // dangling reference
     // {
@@ -44,4 +46,53 @@ fn main() {
     //     let result = String::from("really long string");
     //     result.as_str()
     // }
+
+    struct ImportantExcerpt<'a> {
+        part: &'a str,
+    }
+
+    impl<'a> ImportantExcerpt<'a> {
+        fn label(&self) -> i32 {
+            3
+        }
+
+        fn announce_and_return_part(&self, annoucement: &str) -> &str {
+            println!("Attention please: {}", annoucement);
+            self.part
+        }
+    }
+
+    // struct using lifetime
+    {
+        let novel = String::from("Call me Ishmael. Some years ago...");
+        let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+        let i = ImportantExcerpt {
+            part: first_sentence,
+        };
+        println!("Important Excerpt: {}", i.part);
+
+        println!("Important label: {}", i.label());
+        println!("Important accounce and part is: {}", i.announce_and_return_part("ittokun"));
+    }
+
+    fn longest_with_an_announcement<'a, T>(
+        x: &'a str,
+        y: &'a str,
+        ann: T,
+    ) -> &'a str
+    where
+        T: Display,
+    {
+        println!("Announcement! {}", ann);
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    }
+
+    // generic arguments, trait, lifetime function
+    {
+        println!("longest is: {}", longest_with_an_announcement("hoge", "bar", "baz"));
+    }
 }
