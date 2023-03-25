f0aef8b65b41e816da3451bee985c9f2dd326c8c
ittokun <73953416+ittokun@users.noreply.github.com>
2023-03-26 02:17:03 +0900

    Finished Box<T>

--- projects/smart_pointer/src/main.rs
+++ projects/smart_pointer/src/main.rs
@@ -1,5 +1,6 @@
+#[derive(Debug)]
 enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -15,6 +16,10 @@ fn main() {
     {
         use List::{Cons, Nil};
 
-        let list = Cons(1, Cons(2, Cons(3, Nil)));
+        let list = Cons(1,
+        Box::new(Cons(2,
+            Box::new(Cons(3,
+                Box::new(Nil))))));
+        println!("list = {:?}", list);
     }
 }
