5b4d9edcdbedfb7cad8a88a0e40126e54edbe345
ittokun <73953416+ittokun@users.noreply.github.com>
2023-04-14 01:52:56 +0900

    Updated Threads

--- projects/concurrency/src/main.rs
+++ projects/concurrency/src/main.rs
@@ -2,12 +2,14 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
+
+    handle.join().unwrap();
 
     for i in 1..5 {
         println!("hi number {} from the main thread!", i);
